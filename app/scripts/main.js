7ade035fd2a88b23805e4fc6d8192bfd0ce28c4b
Alex Pate <proto.merkabah@gmail.com>
2015-02-13 09:18:14 -0500

    cleanup and switch to module pattern

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,6 @@
-//(function() {
+(function() {
+
+var chip8 = chip8 || {};
 
 // ROM
 var romBuffer;
@@ -19,35 +21,35 @@ romLoader.send(null);
 
 // Registers v0-vF (0x0 - 0xF)
 // 16 8 bit registers.  VF doubles as carry flag.
-var vBuffer = new ArrayBuffer(0xF),
-    v       = new Uint8Array(vBuffer),
+var vBuffer  = new ArrayBuffer(0xF);
+    chip8.v  = new Uint8Array(vBuffer);
 // Index register 16-bits wide. (address register)
-    I       = 0,
+    chip8.I  = 0;
 // Program Counter 16-bits wide.
-    pc      = 0;
+    chip8.pc = 0;
 
 // Memory 0x000-0xFFF (4096) bytes
 // 0x200 (512) start of application space
 // 0xEA0-0xEFF (3744-3829) callstack
 // 0xF00-0xFFF (3840-4095) display refresh (not used)
-var ramBuffer   = new ArrayBuffer(4096),
-    ram         = new Uint8Array(ramBuffer),
+var ramBuffer   = new ArrayBuffer(4096);
+    chip8.ram   = new Uint8Array(ramBuffer);
     // Stack, homebrew games can expect up to 16 levels (2 bytes per level).
-    stackBuffer = new ArrayBuffer(32), 
-    stack       = new Uint16Array(stackBuffer),
-    sp          = 0;
+    stackBuffer = new ArrayBuffer(32); 
+    chip8.stack = new Uint16Array(stackBuffer);
+    chip8.sp    = 0;
 
 // Timers
 var soundTimer = 0,
     delayTimer = 0;
 
 // Resolution 64x32 (2048 pixels) will display at: 320x160
-var gfxBuffer = new ArrayBuffer(2048),
-    gfx       = new Uint8Array(gfxBuffer);
+var gfxBuffer = new ArrayBuffer(2048);
+    chip8.gfx = new Uint8Array(gfxBuffer);
 
-// Keypad
-var kpBuffer = new ArrayBuffer(16),
-    kp       = new Uint8Array(kpBuffer);
+// Keypad 0x0-0xF (16)
+var kpBuffer = new ArrayBuffer(0xF);
+    chip8.kp = new Uint8Array(kpBuffer);
 
 
 // Cycle: Fetch, Decode, Execute
@@ -57,6 +59,6 @@ var kpBuffer = new ArrayBuffer(16),
 
 
 
+  window.chip8 = chip8;
 
-
-//}());
+}());
