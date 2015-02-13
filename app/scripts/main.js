ce6dae69d1e574b48458373a49cda667699866a8
Alex Pate <proto.merkabah@gmail.com>
2015-02-13 11:34:19 -0500

    start of draw code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -49,14 +49,15 @@ var soundTimer = 0,
 
 // Resolution 64x32 (2048 pixels) will display at: 320x160
 var gfxBuffer = new ArrayBuffer(2048);
-    chip8.gfx = new Uint8Array(gfxBuffer);
+    chip8.gfx = new Uint8Array(gfxBuffer),
+    drawFlag = false;
 
 // Keypad 0x0-0xF (16)
 var kpBuffer = new ArrayBuffer(0xF);
     chip8.kp = new Uint8Array(kpBuffer);
 
 // Font Set
-var fontSet = [ 
+var fontSet = [
   0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
   0x20, 0x60, 0x20, 0x20, 0x70, // 1
   0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -120,7 +121,7 @@ chip8.opL = 0;
 chip8.opCycle = function() {
   // Fetch
   // Opcodes are two bytes, shift left 8 bits, OR in next byte.
-  chip8.opcode = chip8.ram[chip8.pc] << 8 | chip8.ram[chip8.pc + 1]; 
+  chip8.opcode = chip8.ram[chip8.pc] << 8 | chip8.ram[chip8.pc + 1];
   // Decode
   // Take first four bits by masking.
   chip8.opH = chip8.opcode & 0xF000;
@@ -130,17 +131,53 @@ chip8.opCycle = function() {
   // Execute
   // JSPerf says swtich is faster than a jump table...
   switch(chip8.opH) {
-    case 0xA000: // ANNN
-      // Sets I to the address NNN.
-      chip8.I = chip8.opcode & 0x0FFF;
-      chip8.pc += 2;      
-      break;
-    case 0x6000: // 6XNN
+     case 0x6000: // 6XNN
       // Sets VX to NN
       chip8.v[chip8.opcode & 0x0F00] = chip8.opcode & 0x00FF;
       chip8.pc += 2;
       break;
-    default:
+    case 0xA000: // ANNN
+      // Sets I to the address NNN.
+      chip8.I = chip8.opcode & 0x0FFF;
+      chip8.pc += 2;
+      break;
+    case 0xD000: // DXYN
+      // Sprites stored in memory at location in index register (I), maximum 8bits wide. Wraps around the screen.
+      // If when drawn, clears a pixel, register VF is set to 1 otherwise it is zero.
+      // All drawing is XOR drawing (i.e. it toggles the screen pixels)
+      var x = (chip8.opcode & 0x0F00) >> 8,
+          x = chip8.v[x],
+          y = (chip8.opcode & 0x00F0) >> 4,
+          y = chip8.v[y],
+          n = chip8.opcode & 0x000F,
+          pixel;
+
+      // Reset collision flag
+      chip8.v[0xF] = 0;
+
+      var yi,
+          xi,
+          p;
+      for (yi = 0; yi < n; yi++) {
+        pixel = chip8.ram[I + yi];
+        // convert to pixel data, we need 8 bits but JavaScript will only print what is needed.
+        pixel = ("00000000" + pixel.toString(2)).substr(-8);
+        for (xi = 0; xi < pixel.length; xi++) {
+          p = pixel[i] === "1" ? 1 : 0;
+          // Where to draw the pixel.
+          loc = ((y + yi) * 64) + x + xi; 
+
+          // Check for collision
+          if (p === 1 && chip8.gfx[loc] === 1) { chip8.v[0xF] = 1; }
+          // XOR draw;
+          chip8.gfx[loc] ^= 1;
+        }
+      }
+      
+      drawFlag = true;
+      chip8.pc += 2;
+      break;
+   default:
       console.log("Unknown op: ", chip8.opcode.toString(16));
   }
 };
