b79e93237c5d5a852fb02ff1d1b0fd7de8cbcff7
Alex Pate <proto.merkabah@gmail.com>
2015-02-13 15:39:35 -0500

    items draw on screen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,16 +4,17 @@ var chip8 = chip8 || {};
 
 // ROM
 var romBuffer;
-var rom;
+    //chip8.rom;
 var romLoader = new XMLHttpRequest();
 romLoader.onload = function(e) {
   romBuffer = romLoader.response;
-  rom = new Uint8Array(romBuffer);
+  chip8.rom = new Uint8Array(romBuffer);
 
   chip8.init();
   chip8.loadGame();
   console.log("ready");
 };
+//romLoader.open('GET', 'roms/INVADERS', true);
 romLoader.open('GET', 'roms/Breakout.ch8', true);
 romLoader.responseType = 'arraybuffer';
 romLoader.send(null);
@@ -39,7 +40,7 @@ var vBuffer  = new ArrayBuffer(0xF);
 var ramBuffer   = new ArrayBuffer(4096);
     chip8.ram   = new Uint8Array(ramBuffer);
     // Stack, homebrew games can expect up to 16 levels (2 bytes per level).
-    stackBuffer = new ArrayBuffer(32);
+var stackBuffer = new ArrayBuffer(32);
     chip8.stack = new Uint16Array(stackBuffer);
     chip8.sp    = 0;
 
@@ -49,12 +50,13 @@ var soundTimer = 0,
 
 // Resolution 64x32 (2048 pixels) will display at: 320x160
 var gfxBuffer = new ArrayBuffer(2048);
-    chip8.gfx = new Uint8Array(gfxBuffer),
+    chip8.gfx = new Uint8Array(gfxBuffer);
     drawFlag = false;
 
 // Keypad 0x0-0xF (16)
 var kpBuffer = new ArrayBuffer(0xF);
     chip8.kp = new Uint8Array(kpBuffer);
+    chip8.keydown = 0;
 
 // Font Set
 var fontSet = [
@@ -90,24 +92,29 @@ chip8.init = function() {
 
   // According to JSPerf, setting items to 0 is fater than recreating new buffer.
   // Clear stack
-  for (var i = 0; i < chip8.stack.length; i++) { chip8.stack[i] = 0; }
+  var i = 0;
+  for (i = 0; i < chip8.stack.length; i++) { chip8.stack[i] = 0; }
   // Clear memory
-  for (var i = 0; i < chip8.ram.length; i++) { chip8.ram[i] = 0; }
+  for (i = 0; i < chip8.ram.length; i++) { chip8.ram[i] = 0; }
   // Clear registers
-  for (var i = 0; i < chip8.v.length; i++) { chip8.v[i] = 0; }
+  for (i = 0; i < chip8.v.length; i++) { chip8.v[i] = 0; }
   // Clear display
-  for (var i = 0; i < chip8.gfx.length; i++) { chip8.gfx[i] = 0; }
+  for (i = 0; i < chip8.gfx.length; i++) { chip8.gfx[i] = 0; }
+  
+  document.getElementById('chip').addEventListener('keypress', getKeys);
+  document.getElementById('chip').addEventListener('keyup', function() { chip8.keydown = 0; });
 
 };
 
 chip8.loadGame = function() {
   // Font Set should live at 0x50 (80)
-  for (var i = 0; i < fontSet.length; i++) {
+  var i = 0;
+  for (i = 0; i < fontSet.length; i++) {
     chip8.ram[0x50 + i] = fontSet[i];
   }
   // Rom should live at 0x200 (512)
-  for (var i = 0; i < rom.length; i++) {
-    chip8.ram[0x200 + i] = rom[i];
+  for (i = 0; i < chip8.rom.length; i++) {
+    chip8.ram[0x200 + i] = chip8.rom[i];
   }
 };
 
@@ -121,40 +128,89 @@ chip8.opL = 0;
 chip8.opCycle = function() {
   // Fetch
   // Opcodes are two bytes, shift left 8 bits, OR in next byte.
-  chip8.opcode = chip8.ram[chip8.pc] << 8 | chip8.ram[chip8.pc + 1];
+  chip8.opcode = (chip8.ram[chip8.pc] << 8) | chip8.ram[chip8.pc + 1];
+
   // Decode
   // Take first four bits by masking.
   chip8.opH = chip8.opcode & 0xF000;
   // Take the last four bits by masking.
   chip8.opL = chip8.opcode & 0x000F;
 
+  var n   = chip8.opcode & 0x000F,
+      nn  = chip8.opcode & 0x00FF,
+      nnn = chip8.opcode & 0x0FFF,
+      x   = (chip8.opcode & 0x0F00) >> 8,
+      y   = (chip8.opcode & 0x00F0) >> 4;
+
+  console.log(chip8.opcode.toString(16));
   // Execute
   // JSPerf says swtich is faster than a jump table...
   switch(chip8.opH) {
-    case 0x7000: // 7XNN
-      // Adds NN to VX
-      chip8.v[chip8.opcode & 0x0F00] = chip8.opcode & 0x00FF;
+    case 0x0000:
+      switch(chip8.opcode & 0x00FF) {
+        case 0x00EE: // 00EE
+          // Returns from a subroutine
+          chip8.sp -= 1;
+          chip8.pc = chip8.stack[chip8.sp];
+          break;
+
+        default:
+          console.log("Unknown op: ", chip8.opcode.toString(16));
+      }
+      break;
+    case 0x1000: // 1NNN
+      // Jumps to address NNN
+      chip8.pc = nnn;
+      break;
+    case 0x2000: // 2NNN
+      // Calls subroutine at NNN
+      chip8.stack[chip8.sp] = chip8.pc;
+      chip8.sp += 1;
+      chip8.pc = nnn;
+      break;
+    case 0x3000: // 3XNN
+      // Skips the next instruction if VX equals NN
+      if (chip8.v[x] === nn) { chip8.pc += 2; }
       chip8.pc += 2;
       break;
-    case 0x6000: // 6XNN
+   case 0x6000: // 6XNN
       // Sets VX to NN
-      chip8.v[chip8.opcode & 0x0F00] = chip8.opcode & 0x00FF;
+      chip8.v[x] = nn;
       chip8.pc += 2;
+      break;
+    case 0x7000: // 7XNN
+      // Adds NN to VX
+      chip8.v[x] += nn;
+      chip8.pc += 2;
+      break;
+    case 0x8000:
+      switch(chip8.opcode & 0x000F) {
+        case 0x0000: // 8XY0
+          // Sets VX to the value of VY
+          chip8.v[x] = chip8.v[y];
+          chip8.pc += 2;
+          break;
+        default:
+          console.log("Unknown op: ", chip8.opcode.toString(16));
+      }
       break;
     case 0xA000: // ANNN
       // Sets I to the address NNN.
-      chip8.I = chip8.opcode & 0x0FFF;
+      chip8.I = nnn;
       chip8.pc += 2;
       break;
+    //case 0xC000: // CXNN
+      // Sets VX to a random number, masked by NN
+
     case 0xD000: // DXYN
       // Sprites stored in memory at location in index register (I), maximum 8bits wide. Wraps around the screen.
       // If when drawn, clears a pixel, register VF is set to 1 otherwise it is zero.
       // All drawing is XOR drawing (i.e. it toggles the screen pixels)
-      var x = (chip8.opcode & 0x0F00) >> 8,
-          x = chip8.v[x],
-          y = (chip8.opcode & 0x00F0) >> 4,
-          y = chip8.v[y],
-          n = chip8.opcode & 0x000F,
+      var x = (chip8.opcode & 0x0F00) >> 8;
+          x = chip8.v[x];
+      var y = (chip8.opcode & 0x00F0) >> 4;
+          y = chip8.v[y];
+      var n = chip8.opcode & 0x000F,
           pixel;
 
       // Reset collision flag
@@ -170,17 +226,46 @@ chip8.opCycle = function() {
         for (xi = 0; xi < pixel.length; xi++) {
           p = pixel[xi] === "1" ? 1 : 0;
           // Where to draw the pixel.
-          loc = ((y + yi) * 64) + x + xi; 
+          loc = ((y + yi) * 64) + x + xi;
 
           // Check for collision
           if (p === 1 && chip8.gfx[loc] === 1) { chip8.v[0xF] = 1; }
           // XOR draw;
-          chip8.gfx[loc] ^= 1;
+          chip8.gfx[loc] = p;
         }
       }
-      
+
       drawFlag = true;
       chip8.pc += 2;
+      break;
+   case 0xE000:
+      switch(chip8.opcode & 0x000F) {
+        case 0x0001:
+          // Skips the next instruction if the key stored in VX isn't pressed.
+          if (chip8.keydown !== chip8.v[x]) { chip8.pc += 2; }
+          chip8.pc += 2;
+          break;
+        default:
+          console.log("Unknown op: ", chip8.opcode.toString(16));
+      }
+      break;
+   case 0xF000:
+      switch(chip8.opcode & 0x00FF) {
+        case 0x001E: // FX1E
+          // Adds VX to I  
+          chip8.I += chip8.v[x]; 
+          chip8.pc += 2;
+          break;
+        case 0x0065: // FX65
+          // Fills V0 to VX with values from memory starting at address I
+          for (var i = 0; i <= x; i++) {
+            chip8.v[i] = chip8.ram[chip8.I + i];
+          }
+          chip8.pc += 2;
+          break;
+        default: 
+          console.log("Unknown op: ", chip8.opcode.toString(16));
+      }
       break;
    default:
       console.log("Unknown op: ", chip8.opcode.toString(16));
@@ -197,7 +282,7 @@ chip8.opCycle = function() {
   var ctx = document.getElementById('screen').getContext('2d');
   chip8.render = function() {
     var w  = 320,
-        y  = 160,
+        h  = 160,
         xp = 32,
         yp = 64;
 
@@ -206,21 +291,30 @@ chip8.opCycle = function() {
     for (var i = 0; i < 2048; i++) {
       var x = i % 64,
           y = (i / 64) << 0;
-      if (chip8.gfx[i] === 1) { ctx.fillRect(x + 5, y + 5, 5, 5); }
-    };
+      if (chip8.gfx[i] === 1) { ctx.fillRect(x * 5, y * 5, 5, 5); }
+    }
 
     drawFlag = false;
-  }
-
-
-  chip8.loop = function() {
-    chip8.opCycle(); 
-    if (drawFlag === true) { chip8.render(); }
   };
 
+  function getKeys(e) {
+    if (e.which) { 
+      chip8.keydown = e.which;
+    } else {
+      chip8.keydown = 0;
+    }
 
+  }
+  var animationID;
+  chip8.loop = function() {
+    chip8.opCycle();
+    if (drawFlag === true) { chip8.render(); }
+    animationID = requestAnimationFrame(chip8.loop);
+  };
 
-
+  chip8.stop = function() {
+    cancelAnimationFrame(animationID);
+  };
 
   window.chip8 = chip8;
 
